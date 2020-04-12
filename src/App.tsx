f7bb8b0e28d404494e7dedb25b52cfe5757960fd
mattandayo <mattandayo@gmail.com>
2020-04-12 18:17:22 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const logoAttrs = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo,
+    };
+    const title = "こんにちはReact";
+    const targets = ["World", "Kanae", "Yukina"];
+
+    return (
+      <>
+        { title && <p>{title}</p> }
+        <div className="App">
+          <header className="App-header">
+            {
+              // コメントはこう書く
+            }
+            <img {...logoAttrs} />
+            {title && <p>{title}</p>}
+            {targets.map((target) => (
+              <p>Hello, {target}!</p>
+            ))}
+          </header>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
