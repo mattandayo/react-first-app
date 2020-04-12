cb6e624521c0bdddc06f6612527d6c520ac0aedf
mattandayo <mattandayo@gmail.com>
2020-04-12 23:13:40 +0900

    rebuild app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+/* eslint-disable react/jsx-key, react/jsx-props-no-spreading, jsx-a11y/alt-text */
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     const logoAttrs = {
-      alt: "logo",
-      className: "App-logo",
+      alt: 'logo',
+      className: 'App-logo',
       src: logo,
     };
-    const title = "こんにちはReact";
-    const targets = ["World", "Kanae", "Yukina"];
+    const title = 'こんにちはReact';
+    const targets = ['World', 'Kanae', 'Yukina'];
 
     return (
-      <>
-        { title && <p>{title}</p> }
-        <div className="App">
-          <header className="App-header">
-            {
-              // コメントはこう書く
-            }
-            <img {...logoAttrs} />
-            {title && <p>{title}</p>}
-            {targets.map((target) => (
-              <p>Hello, {target}!</p>
-            ))}
-          </header>
-        </div>
-      </>
-    )
+      <div className="App">
+        <header className="App-header">
+          {
+            // コメントはこう書く
+          }
+          <img {...logoAttrs} />
+          {title && <p>{title}</p>}
+          {targets.map(target => (
+            <p>Hello, {target}!</p>
+          ))}
+        </header>
+      </div>
+    );
   }
 }
 
